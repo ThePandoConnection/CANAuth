4061c54567a52f0fc633250ee15ece983a142fff
ThePandoConnection <lewiso.3101@gmail.com>
2024-03-27 17:59:52 +0000

    set up both threads, I think i will share data with a txt or csv but i need to make sure its thread safe.

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ class voltageThread(threading.Thread):
         getVoltage(self.port, self.baudrate)
         print('Monitoring complete')
 
+class messageThread(threading.Thread):
+    def __init__(self, port, baudrate):
+        threading.Thread.__init__(self)
+        self.port = port
+        self.baudrate = baudrate
+
+    def run(self):
+        print('Starting Monitoring')
+        getMessage(self.port, self.baudrate)
+        print('Monitoring complete')
+
 
 
 def getVoltage(port, baudrate):
@@ -24,9 +35,20 @@ def getVoltage(port, baudrate):
         print(line)
     ser.close()
 
+def getMessage(port, baudrate):
+    ser = serial.Serial(port, baudrate)
+    ser.open()
+    print("Opened serial port")
+    for line in ser.read():
+        print(line)
+    ser.close()
+
+
 def main():
-    thread = voltageThread("COM13", 9600)
-    thread.start()
+    threadVolt = voltageThread("COM13", 9600)
+    threadVolt.start()
+    threadMessage = messageThread("COM8", 9600)
+    threadMessage.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
