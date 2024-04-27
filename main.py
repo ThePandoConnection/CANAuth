19d40e5b1d43f6ffedac554b1ab343a42d18af3e
ThePandoConnection <lewiso.3101@gmail.com>
2024-04-27 18:32:40 +0100

    code now functioning mostly, need to test values to get the length. Completely rewritten but legacy thread stuff needs removed.

--- main.py
+++ main.py
@@ -4,82 +4,76 @@ from threading import Thread, Event
 from queue import Queue
 
 class voltageThread(Thread):
-    def __init__(self, port, baudrate, record, stop, share):
+    def __init__(self, port, baudrate, share):
         Thread.__init__(self)
         self.port = port
         self.baudrate = baudrate
-        self.record = record
-        self.stop = stop
         self.share = share
 
     def run(self):
         print('Starting Monitoring')
-        getVoltage(self.port, self.baudrate, self.record, self.stop, self.share)
+        getVoltage(self.port, self.baudrate, self.share)
         print('Monitoring complete')
 
 class messageThread(Thread):
-    def __init__(self, port, baudrate, record, stop, share):
+    def __init__(self, port, baudrate, share):
         Thread.__init__(self)
         self.port = port
         self.baudrate = baudrate
-        self.record = record
-        self.stop = stop
         self.share = share
 
     def run(self):
         print('Starting Monitoring')
-        getMessage(self.port, self.baudrate, self.record, self.stop, self.share)
+        getMessage(self.port, self.baudrate, self.share)
         print('Monitoring complete')
 
 
 
-def getVoltage(port, baudrate, record, stop, share):
-    while True:
-        if record.is_set():
-            recording = True
-            node = share.get()
-            print(node)
-            ser = serial.Serial(port, baudrate)
-            print("Opened voltage serial port")
-            domBits = []
-            while recording:
-                line = ser.readline().decode()
-                print(line)
-                if float(line) < 25000:
-                    domBits.append(line)
-                if stop.is_set():
-                    recording = False
-            ser.close()
-            print(domBits)
-            average = sum(domBits) / len(domBits)
-            length = 170*(average - 2.25 * 10**4)/167
-            print('The wire length is: ' + length)
+def getVoltage(port, baudrate):
+    return 0
 
-def getMessage(port, baudrate, record, stop, share):
+
+
+
+def getMessage(port, baudrate, share):
     ser = serial.Serial(port, baudrate)
     print("Opened message serial port")
     #ser.open()
     while True:
-        line = int(ser.readline().decode().strip(" "))
+        line = ser.readline().decode().strip(" ")
         print(line)
-        if line == 100:
-            print('Stopping Voltage Monitoring')
-            stop.set()
-            record.clear()
-        else:
-            share.put(line)
-            record.set()
-    #ser.close()
+    ser.close()
 
 
 def main():
-    record = Event()
-    stop = Event()
     share = Queue()
-    threadMessage = messageThread("COM6", 115200, record, stop, share)
-    threadMessage.start()
-    threadVolt = voltageThread("COM3", 9600, record, stop, share)
-    threadVolt.start()
+
+    serV = serial.Serial("COM3", 9600)
+    serM = serial.Serial("COM6", 115200)
+    print("Opened Voltage serial port")
+    print("Opened Message serial port")
+    voltList = []
+    # ser.open()
+    while True:
+        try:
+            line = float(serV.readline().decode().strip())
+            if line < 25000:
+                recording = True
+                while recording:
+                    lineLow = float(serV.readline().decode().strip())
+                    if lineLow < 25000:
+                        voltList.append(lineLow)
+                    else:
+                        recording = False
+                print(serM.readline().decode().strip())
+                averageVolt = sum(voltList) / len(voltList)
+                length = 170 * (averageVolt - 2.25 * 10 ** 4) / 167
+                print('wire length is: ' + str(length), flush=True)
+                voltList = []
+
+        except ValueError:
+            print("Value Error")
+
 
 
 # Press the green button in the gutter to run the script.
