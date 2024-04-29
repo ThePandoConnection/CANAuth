9f0ef4f1b95496b91f248d08e21caf0a2d41b3d6
ThePandoConnection <lewiso.3101@gmail.com>
2024-04-29 14:39:47 +0100

    removed all the old multi threading code. still trying to figure out correct equation.

--- main.py
+++ main.py
@@ -3,47 +3,6 @@ import time
 from threading import Thread, Event
 from queue import Queue
 
-class voltageThread(Thread):
-    def __init__(self, port, baudrate, share):
-        Thread.__init__(self)
-        self.port = port
-        self.baudrate = baudrate
-        self.share = share
-
-    def run(self):
-        print('Starting Monitoring')
-        getVoltage(self.port, self.baudrate, self.share)
-        print('Monitoring complete')
-
-class messageThread(Thread):
-    def __init__(self, port, baudrate, share):
-        Thread.__init__(self)
-        self.port = port
-        self.baudrate = baudrate
-        self.share = share
-
-    def run(self):
-        print('Starting Monitoring')
-        getMessage(self.port, self.baudrate, self.share)
-        print('Monitoring complete')
-
-
-
-def getVoltage(port, baudrate):
-    return 0
-
-
-
-
-def getMessage(port, baudrate, share):
-    ser = serial.Serial(port, baudrate)
-    print("Opened message serial port")
-    #ser.open()
-    while True:
-        line = ser.readline().decode().strip(" ")
-        print(line)
-    ser.close()
-
 
 def main():
     share = Queue()
@@ -68,7 +27,7 @@ def main():
                 print(serM.readline().decode().strip())
                 averageVolt = sum(voltList) / len(voltList)
                 length = 170 * (averageVolt - 2.25 * 10 ** 4) / 167
-                print('wire length is: ' + str(length), flush=True)
+                print('wire length is: ' + str(length))
                 voltList = []
 
         except ValueError:
